9754591a92d921901e7d85c0b935c4609c98612d
eltajj18 <amirli.eltaj18@gmail.com>
2025-06-07 23:38:50 +0200

    add

--- nslookup.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse("/home/eltaj-amirli/Downloads/koralLocalConfig.xml")
-root = tree.getroot()
-for prop in root.findall('property'):
-    name = prop.find('name').text
-    if name == 'master':
-        x=(prop.find('value').text)
-        print(x)
-        
